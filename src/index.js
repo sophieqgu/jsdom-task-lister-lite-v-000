895fff652373611b33dd08141b69deab5d9572ff
Sophie Gu <sophieqgu@gmail.com>
2021-01-05 04:29:49 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form =  document.getElementById('create-task-form');
+
+  function createTask(event){
+      const newListItem = document.createElement('li');
+      newListItem.textContent = form.querySelector('#new-task-description').value;
+
+      document.getElementById('tasks').appendChild(newListItem);
+      event.preventDefault();
+      event.target.reset();
+    }
+
+  form.addEventListener('submit',createTask);
 });
