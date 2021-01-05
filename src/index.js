7c35230450470101aad7460623796ec27571805b
Sophie Gu <sophieqgu@gmail.com>
2021-01-05 04:18:52 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const form = document.getElementByID('create-task-form');
-  form.add()
+
+  function createTask(event) {
+      const newListItem = document.createElement('li');
+      newListItem.textContent = form.querySelector('new-task-description').value
+  }
+
+  form.addEventListener('submit', createTask);
 });
