e1638ca0698bc2cafca8fd885521e4a0f9a039c7
Sophie Gu <sophieqgu@gmail.com>
2021-01-05 04:27:19 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const form =  document.getElementById('create-task-form');
 
-function createTask(event){
-    const createNewLi = document.createElement('li');
-    createNewLi.textContent = form.querySelector('#new-task-description').value;
+  function createTask(event){
+      const newListItem = document.createElement('li');
+      newListItem.textContent = form.querySelector('#new-task-description').value;
 
-    document.getElementById('tasks').appendChild(createNewLi);
-    event.preventDefault();
-    event.target.reset();
-  }
+      document.getElementById('tasks').appendChild(newListItem);
+      event.preventDefault();
+      event.target.reset();
+    }
 
   form.addEventListener('submit',createTask);
 });
